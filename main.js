af5ad672089b6d7a35bb8863e32c4aa2a574736e
Eric Kim <cyon256@icloud.com>
2022-03-11 15:58:49 +0900

    code

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
-const BMICalculator = (height, weight) => {
-  const BMI = height / (weight * weight);
-  if (BMI < 19.5) {
-    return console.log("You are too under average");
-  } else if (BMI < 23 && BMI > 18.5) {
-    return console.log("You are on average");
-  } else {
-    return console.log("You are above average");
-  }
+const calculator = () => {
+  let a;
+
+  return a;
 };
 
-BMICalculator(165, 60);
+if (calculator()) {
+  console.log("hi");
+} else {
+  console.log("bye");
+}
